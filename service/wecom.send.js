b79ede360c9cd7c7597bece6f70c04f3b62714d7
litie <litie@foxmail.com>
2023-05-03 14:09:07 +0800

    1##

--- service/wecom.send.js
+++ service/wecom.send.js
@@ -11,24 +11,28 @@ const {
 } = require('../config')
 const axios = require('axios')
 const router = express.Router()
-
+var reply_msg = ''
 router.all('/wecom/send', async ({ query: { string, user, type, retry } }, response) => {
   try {
     const redisclient = createClient({ url: 'redis://127.0.0.1:6379' });
     redisclient.on('error', err => console.log('Redis Client Error', err));
     await redisclient.connect();
-    const access_token = await redisclient.get(`${wecomCorpid}_access_token`);
+    let access_token = await redisclient.get(`${wecomCorpid}_access_token`);
     if (!access_token) {
-      const {
-        data: { access_token }
-      } = await axios.get(
+      const token_res = await axios.get(
         `${WECOM_BASE_URL}/cgi-bin/gettoken?corpid=${wecomCorpid}&corpsecret=${wecomSecret}`
       )
+      access_token = token_res.data.access_token
       await redisclient.set(`${wecomCorpid}_access_token`, access_token)
       await redisclient.expire(`${wecomCorpid}_access_token`, 60 * 60 * 1.8)
     }
     console.log(`wx Access Token: ${access_token}`)
-    const message = await touchOpenAI(string, user)
+    let message = ''
+    if (!reply_msg) {
+      message = await touchOpenAI(string, user)
+    } else {
+      message = reply_msg
+    }
     console.log(`Reply content: ${message}`)
     const status = await axios.post(
       `${WECOM_BASE_URL}/cgi-bin/message/send?access_token=${access_token}`,
@@ -41,11 +45,12 @@ router.all('/wecom/send', async ({ query: { string, user, type, retry } }, respo
         }
       }
     )
+    reply_msg = message
     console.log(`Send Status: ${JSON.stringify(status.data)}`)
     if ([40014, 42201, 42001, 42009, 42007].includes(status.data.errcode)) {
       await redisclient.del(`${wecomCorpid}_access_token`)
+      console.log('wecom send error:', status.data)
       if (!retry) {
-        console.log('wecom send error:', status.data)
         console.log('retry......')
         await axios.get(`${TOOLBOX_BASE_URL}/wecom/send?string=${string}&user=${user}&type=${type}&retry=true`)
       }
